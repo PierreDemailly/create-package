bfcc762099a9d323015061de5dda5ac983c9bf65
PierreDemailly <pierredemailly.pro@gmail.com>
2023-06-15 00:22:38 +0200

    improve fileURLToPath usage

--- src/license.js
+++ src/license.js
@@ -8,14 +8,14 @@ import { Feature } from "./feature.js";
 import { gitAuthor } from "./utils.js";
 
 // CONSTANTS
-const kLicensePath = join(import.meta.url, "../assets/licenses/MIT");
+const kLicensePath = join(fileURLToPath(import.meta.url), "../assets/licenses/MIT");
 
 export async function license() {
   const feature = new Feature();
   // TODO: maybe in the future handle multiple licenses, interactively
   feature.files.push({
     path: "LICENSE",
-    content: readFileSync(fileURLToPath(kLicensePath), "utf8")
+    content: readFileSync(kLicensePath, "utf8")
       .replace("[year]", new Date().getFullYear())
       .replace("[fullname]", gitAuthor())
   });
