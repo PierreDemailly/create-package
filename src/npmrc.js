738895e9d677a610e497b7407d818e8ff7b9f040
PierreDemailly <pierredemailly.pro@gmail.com>
2025-01-13 20:16:33 +0100

    feat: add yes option

--- src/npmrc.js
+++ src/npmrc.js
@@ -4,8 +4,12 @@ import { confirm } from "@topcli/prompts";
 // Import Internal Dependencies
 import { projectConfig } from "./projectConfig.js";
 
-export async function npmrc() {
-  const packageLock = await confirm("Keep package-lock.json ?", { initial: false });
+export async function npmrc(options = {}) {
+  const { yes = false } = options;
+  const packageLock = await confirm("Keep package-lock.json ?", {
+    initial: false,
+    skip: yes
+  });
 
   if (!packageLock) {
     projectConfig.files.push({
